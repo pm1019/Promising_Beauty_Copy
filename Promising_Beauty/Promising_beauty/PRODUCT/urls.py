41d94a0aa7b64ce4a1921d09ce8f5152eb801ac4
PBCOPY <promisingbeauty131@gmail.com>
2023-02-08 06:20:38 +0530

    PINKY/INBUILT

--- Promising_Beauty/Promising_beauty/PRODUCT/urls.py
+++ Promising_Beauty/Promising_beauty/PRODUCT/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('product',views.product,name="shop"),
-    path('<int:id>',views.prods_detail)
+    path('<int:id>',views.prods_detail),
     path('Addtocart',views.add_to_cart,name='CART'),
     path('add',views.show_cart,name='add')
     
